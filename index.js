5106e47e4a34863b42a72ddbdaced252e1c85283
vangitech <softwareking101@gmail.com>
2025-02-03 18:10:53 +0100

    update the code is cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const todoRoutes = require("./routes/todoRoutes")
 const userRoutes = require("./routes/userRoutes")
+const cors = require("cors")
 
 const local_uri ="mongodb://localhost:27017/TODO_APP"
 const live_uri = "mongodb+srv://Esther:Esther2004@cluster0.byfqhoj.mongodb.net/TODO_APP?retryWrites=true&w=majority&appName=Cluster0"
@@ -16,6 +17,7 @@ mongoose.connect(live_uri).then(()=>{
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use("/todo" , todoRoutes)
 app.use("/user" , userRoutes)
 
